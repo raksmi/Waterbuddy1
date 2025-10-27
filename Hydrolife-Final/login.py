f6f16850e5abb7dcb097edd111e7a18e7fab316e
Raksmi~ <raksmi@jainvidyalaya.in>
2025-10-27 12:44:21 +0530

    Update login.py

--- Hydrolife-Final/login.py
+++ Hydrolife-Final/login.py
@@ -8,9 +8,9 @@ from database import user_exists, verify_user, get_all_usernames
 def show_login():
     """Show login/signup page"""
     st.markdown("""
-    <div style="text-align: center; padding: 25px 0;">
+    <div style="text-align: center; padding: 15px 0;">
         <div style="font-size: 80px; margin-bottom: 0px;">💧</div>
-        <h1 style="color: white; font-family: Arial, sans-serif; margin-top: 0; margin-bottom: 4px;">
+        <h1 style="color: white; font-family: Arial, sans-serif; margin-top: 0; margin-bottom: 0px;">
             Welcome to HydroLife!
         </h1>
         <p style="color: black; font-size: 18px;">Your personal hydration companion</p>
